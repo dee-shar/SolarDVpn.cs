a0278f427f6b6b4d22addfefc729b75edee60fa2
huok <77536370+dee-shar@users.noreply.github.com>
2025-10-25 20:23:26 +0300

    Update and rename solar_dvpn.sh to SolarDVpn.cs

---
+++ src/SolarDVpn.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolarDVpnApi
+{
+    public class SolarDVpn
+    {
+        private readonly HttpClient httpClient;
+        private readonly string apiUrl = "https://api.dvpn.solar/api";
+        public SolarDVpn()
+        {
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("okhttp/4.7.2");
+            httpClient.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+        public async Task<string> getDeviceToken()
+        {
+            var jsonContent = JsonContent.Create(new {platform = "ANDROID"});
+            var response = await httpClient.PostAsync($"{apiUrl}/device", jsonContent);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+        public async Task<string> getCurrentIp()
+        {
+            var response = await httpClient.GetAsync($"{apiUrl}/vpn/ip");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<string> getCountries()
+        {
+            var response = await httpClient.GetAsync($"{apiUrl}/vpn/countries");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
